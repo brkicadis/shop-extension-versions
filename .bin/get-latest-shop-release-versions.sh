cbe4f1e5b4ba6b41886ed14c9f6411f9f88847b2
brkicadis <adis.brkic@wirecard.com>
2020-03-18 15:59:27 +0100

    Update name

--- .bin/get-latest-shop-release-versions.sh
+++ .bin/get-latest-shop-release-versions.sh
@@ -4,24 +4,14 @@ set -e
 set -x
 
 
-#DEFAULT_CONDITION=$(jq -r '.[] | .tag_name' | egrep -v [a-zA-Z] | head -3)
-FILENAME=${SHOP_SYSTEM^^}_COMPATIBILITY_FILE
-#
-#case ${SHOP_SYSTEM} in "prestashop") export CONDITION=jq -r '.[] | .tag_name' | egrep -v [a-zA-Z] | grep -v "^1.6";;
-#"woocommerce") export CONDITION=${DEFAULT_CONDITION};;
-#"magento2") export CONDITION=${DEFAULT_CONDITION};;
-#*)
-#esac
-echo "Filename: "
-echo "${FILENAME}"
 curl -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/repos/"${SHOP_SYSTEM}"/"${SHOP_SYSTEM}"/releases | jq -r '.[] | .tag_name' | egrep -v [a-zA-Z] | head -3 > tmp.txt
 
-sort -nr tmp.txt > "${FILENAME}"
+sort -nr tmp.txt > "${COMPATIBILITY_FILE}"
 
-if [[ $(git diff HEAD "${FILENAME}") != '' ]]; then
+if [[ $(git diff HEAD "${COMPATIBILITY_FILE}") != '' ]]; then
     git config --global user.name "Travis CI"
     git config --global user.email "wirecard@travis-ci.org"
-    git add  "${FILENAME}"
+    git add  "${COMPATIBILITY_FILE}"
     git commit -m "${SHOP_SYSTEM_UPDATE_COMMIT}"
     cd /home/travis/build/wirecard/prestashop-ee/shop-extension-versions && git push https://"${GITHUB_TOKEN}"@github.com/brkicadis/shop-extension-versions.git HEAD:TPWDCEE-5954
 fi
--- .bin/update-shop-release-versions.sh
+++ .bin/update-shop-release-versions.sh
@@ -3,14 +3,11 @@
 set -e
 set -x
 
-COMPATIBILITY_FILENAME=${SHOP_SYSTEM^^}_COMPATIBILITY_FILE
-RELEASE_FILENAME=${SHOP_SYSTEM^^}_COMPATIBILITY_FILE
-
 if [[ ${COMPATIBILITY_CHECK}  == "1" ]]; then
-    cp "${COMPATIBILITY_FILENAME}" "${RELEASE_FILENAME}"
+    cp "${COMPATIBILITY_FILE}" "${RELEASES_FILE}"
     git config --global user.name "Travis CI"
     git config --global user.email "wirecard@travis-ci.org"
-    git add  "${RELEASE_FILENAME}"
+    git add  "${RELEASES_FILE}"
     git commit -m "${SHOP_SYSTEM_UPDATE_COMMIT}"
     cd /home/travis/build/wirecard/prestashop-ee/shop-extension-versions && git push https://"${GITHUB_TOKEN}"@github.com/brkicadis/shop-extension-versions.git HEAD:TPWDCEE-5954
 fi
