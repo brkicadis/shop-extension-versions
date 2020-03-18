fa16f199cd3d2485ccd455e2bd88b165ff7abdf0
brkicadis <adis.brkic@wirecard.com>
2020-03-18 16:04:14 +0100

    Update name

--- .bin/get-latest-shop-release-versions.sh
+++ .bin/get-latest-shop-release-versions.sh
@@ -13,5 +13,5 @@ if [[ $(git diff HEAD "${COMPATIBILITY_FILE}") != '' ]]; then
     git config --global user.email "wirecard@travis-ci.org"
     git add  "${COMPATIBILITY_FILE}"
     git commit -m "${SHOP_SYSTEM_UPDATE_COMMIT}"
-    cd /home/travis/build/wirecard/prestashop-ee/shop-extension-versions && git push https://"${GITHUB_TOKEN}"@github.com/brkicadis/shop-extension-versions.git HEAD:TPWDCEE-5954
+    cd /home/travis/build/wirecard/prestashop-ee/shop-extension-versions && git push https://"${GITHUB_TOKEN}"@github.com/brkicadis/shop-extension-versions.git HEAD:master
 fi
